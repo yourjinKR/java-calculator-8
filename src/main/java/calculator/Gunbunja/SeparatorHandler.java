e2724f77a5c587d5e0163637592edd8540123282
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-10-15 19:29:47 +0900

    feat(SeparatorHandler): 계산 영역에 구분자와 숫자 외 다른 문자가 있는지 검증
    인스턴스 변수로 저장한 커스텀 구분자를 정규식에 추가해 검증

--- src/main/java/calculator/Gunbunja/SeparatorHandler.java
+++ src/main/java/calculator/Gunbunja/SeparatorHandler.java
@@ -2,7 +2,7 @@ package calculator.Gunbunja;
 
 
 public class SeparatorHandler {
-    private String customSeparator;
+    private String customSeparator = "";
     private String calcArea;
 
     public boolean validateGubunJa(String solution) {
@@ -16,5 +16,9 @@ public class SeparatorHandler {
 
         this.customSeparator = customSeparator;
         this.calcArea = calcArea;
+    }
+
+    public boolean validateCalcArea() {
+        return calcArea.matches("^[0-9,:" + customSeparator + "]*$");
     }
 }
