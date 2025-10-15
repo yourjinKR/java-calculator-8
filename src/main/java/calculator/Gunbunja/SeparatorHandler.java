9b822d50c5cf7bdef1a63684ec81562f48609e52
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-10-15 19:11:46 +0900

    feat(SeparatorHandler): 커스텀 구분자 문구와 계산 영역을 분리

--- src/main/java/calculator/Gunbunja/SeparatorHandler.java
+++ src/main/java/calculator/Gunbunja/SeparatorHandler.java
@@ -2,11 +2,19 @@ package calculator.Gunbunja;
 
 
 public class SeparatorHandler {
+    private String customSeparator;
+    private String calcArea;
 
     public boolean validateGubunJa(String solution) {
         String customSeparator = solution.substring(0,5);
         return customSeparator.matches("^//.\\\\n");
     }
 
+    public void splitByGubunJa(String solution) {
+        String customSeparator = solution.substring(0, 5);
+        String calcArea = solution.substring(5);
 
+        this.customSeparator = customSeparator;
+        this.calcArea = calcArea;
+    }
 }
